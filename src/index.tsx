0a45ac8388832acdb7c41b3f0c95742c32efd5a4
William Fortin <willyfortin@gmail.com>
2019-01-17 14:12:42 -0500

    HOC

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { render } from 'react-dom'
 import './styles/reset.css'
 import './styles/global.css'
 
-import App from './components/app'
+import App from './components/app-with-loading'
 
 const rootEl = document.getElementById('app')
 

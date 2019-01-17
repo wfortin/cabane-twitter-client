eb2e32f14ee254801cc2999a59ec9efa1e8232f0
William Fortin <willyfortin@gmail.com>
2019-01-17 14:17:38 -0500

    Render Props

--- src/components/tweet/tweet.tsx
+++ src/components/tweet/tweet.tsx
@@ -2,24 +2,24 @@ import moment from 'moment'
 import React, { Component } from 'react'
 
 import Avatar from '../avatar/avatar'
-import Retweet from '../retweet/retweet'
-import Favorite from '../favorite/favorite'
+import Retweet from '../retweet/retweet-render-prop'
+import Favorite from '../favorite/favorite-render-prop'
 import Media from '../media/media'
 import * as API from '../../api/types'
+import Moment from '../moment/moment'
+import Toggle from '../toggle/toggle-render-prop'
 
 import styles from './tweet.css'
 
 export default class Tweet extends Component<API.Tweet, {}> {
-  formatDate = () =>
-    moment(this.props.created_at, 'ddd MMM DD HH:mm:ss ZZ YYYY').format('LL')
-
   render() {
     const {
       user,
       full_text,
       retweet_count,
       favorite_count,
-      entities
+      entities,
+      created_at
     } = this.props
 
     return (
@@ -32,8 +32,13 @@ export default class Tweet extends Component<API.Tweet, {}> {
           <div className={styles.col}>
             <div className={styles.row}>
               <span className={styles.name}>{user.name}</span>
-              <span className={styles.handle}>{user.screen_name}</span>!
-              <span className={styles.date}>{this.formatDate()}</span>
+              <span className={styles.handle}>{user.screen_name}</span>
+              <Moment
+                date={created_at}
+                render={({ formattedDate }) => (
+                  <span className={styles.date}>{formattedDate}</span>
+                )}
+              />
             </div>
             <div className={styles.row}>{full_text}</div>
           </div>
@@ -42,8 +47,26 @@ export default class Tweet extends Component<API.Tweet, {}> {
           <Media media={entities.media} />
         </div>
         <div className={styles.row}>
-          <Retweet retweet_count={retweet_count} />
-          <Favorite favorite_count={favorite_count} />
+          <Toggle
+            initialValue={this.props.retweeted}
+            render={(retweeted, toggleRetweet) => (
+              <Retweet
+                retweeted={retweeted}
+                retweet_count={retweeted ? retweet_count + 1 : retweet_count}
+                onClick={toggleRetweet}
+              />
+            )}
+          />
+          <Toggle
+            initialValue={this.props.favorited}
+            render={(favorited, toggleFavorite) => (
+              <Favorite
+                favorited={favorited}
+                favorite_count={favorited ? favorite_count + 1 : favorite_count}
+                onClick={toggleFavorite}
+              />
+            )}
+          />
         </div>
       </div>
     )
